9ab02c3d0e43af281501abf0292448cf25c39b8b
negineri <harusoin@gmail.com>
2020-02-25 17:19:06 +0900

    First commit

---
+++ escape.rb
@@ -0,0 +1,11 @@
+class FSEscape
+  def self.escape(path, escape_char = '_')
+      reserved_words_windows = Set.new([
+          "CON", "PRN", "AUX", "NUL",
+          "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
+          "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"
+      ])
+      return "#{escape_char}#{path}" if reserved_words_windows.include?(File.basename(path, ".*").upcase)
+      path.gsub(%r{[/\\?%*:|"<>. ]}, escape_char)
+  end
+end
---
+++ main.rb
@@ -0,0 +1,146 @@
+require 'net/http'
+require 'json'
+require 'uri'
+require './escape'
+require 'optparse'
+
+class FanboxItems
+  def initialize(user_id,cookies)
+    @user_id = user_id
+    @cookies = cookies
+    @query_parameters = {"userId"=>user_id, "maxPublishedDatetime"=>Time.now.strftime("%Y-%m-%d %H:%M:%S"), "maxId"=>9999999, "limit"=>10}
+    @savedataDir = "data"
+    @random = Random.new
+  end
+  def get_all
+    creator_info = JSON.parse(get_raw("https://www.pixiv.net/ajax/fanbox/creator?userId=#{@user_id}"))
+    postlist = creator_info['body']['post']
+    while loop
+      sleep(@random.rand(1.0)+1)
+      postlist["items"].each do |item|
+        savedataDirPath = "#{@savedataDir}/#{@user_id}/posts/#{item['id']}_#{FSEscape.escape(item['title'])}/"
+        next unless Dir.glob("#{savedataDirPath}").empty?
+        sleep(@random.rand(1.0)+1)
+        FileUtils.mkdir_p(savedataDirPath)
+        next if item['body'] == nil
+        case item['type']
+        when "image"
+          if item['body']['text'] != ""
+            open("#{savedataDirPath}/article.txt", 'w') do |output|
+              output.puts item['body']['text']
+            end
+          end
+          if item['coverImageUrl']
+            open("#{savedataDirPath}/cover.jpeg", 'wb') do |output|
+              output.write(get_raw(item['coverImageUrl']))
+            end
+          end
+          item['body']['images'].each_with_index do |filedata, i|
+            open(sprintf("%s/%04d.%s",savedataDirPath,i,filedata['extension']), 'wb') do |output|
+              output.write(get_raw(filedata['originalUrl']))
+            end
+          end
+        when "article"
+          if item['coverImageUrl']
+            open("#{savedataDirPath}/cover.jpeg", 'wb') do |output|
+              output.write(get_raw(item['coverImageUrl']))
+            end
+          end
+          article = ""
+          item['body']['blocks'].each_with_index do |data, i|
+            case data['type']
+            when "p"
+              article = "#{article}#{data['text']}  \n"
+            when "image"
+              image_data = item['body']['imageMap'][data['imageId']]
+              article = "#{article}#{sprintf("<img src=\"%04d.%s\" alt=\"\" width=\"%d\" height=\"%d\"><br>\n",i,image_data['extension'],image_data['width'],image_data['height'])}"
+              open(sprintf("%s/%04d.%s",savedataDirPath,i,image_data['extension']),'wb') do |output|
+                output.write(get_raw(image_data['originalUrl']))
+              end
+            else
+              p "ERROR: undefined article data type (#{item['id']})"
+            end
+          end
+          open("#{savedataDirPath}/article.md", 'w') do |output|
+            output.puts article
+          end
+        when "text"
+          if item['coverImageUrl']
+            open("#{savedataDirPath}/cover.jpeg", 'wb') do |output|
+              output.write(get_raw(item['coverImageUrl']))
+            end
+          end
+          if item['body']['text'] != ""
+            open("#{savedataDirPath}/article.txt", 'w') do |output|
+              output.puts item['body']['text']
+            end
+          end
+        when "file"
+          if item['coverImageUrl']
+            open("#{savedataDirPath}/cover.jpeg", 'wb') do |output|
+              output.write(get_raw(item['coverImageUrl']))
+            end
+          end
+          if item['body']['text'] != ""
+            open("#{savedataDirPath}/article.txt", 'w') do |output|
+              output.puts item['body']['text']
+            end
+          end
+          item['body']['files'].each_with_index do |filedata, i|
+            open(sprintf("%s/%s.%s",savedataDirPath,filedata['name'],filedata['extension']), 'wb') do |output|
+              output.write(get_raw(filedata['url']))
+            end
+          end
+        else
+          p "ERROR: undefined post type (#{item['id']})"
+        end
+      end
+      p postlist['nextUrl']
+      break unless postlist['nextUrl']
+      postlist = JSON.parse(get_raw(postlist['nextUrl']))['body']
+    end
+  end
+  private
+  def get_raw(url)
+    uri = URI.parse(url)
+    req = Net::HTTP::Get.new(uri.request_uri)
+    req['origin'] = 'https://www.pixiv.net'
+    req['cookie'] = @cookies.map{|k,v|
+      "#{k}=#{v}"
+    }.join(';')
+    request_options = {
+      use_ssl: uri.scheme
+    }
+    res = Net::HTTP.start(uri.host, uri.port, request_options) do |http|
+      http.request(req)
+    end
+    res.body
+  end
+end
+
+option={}
+OptionParser.new do |opt|
+    opt.on("-s", "--session=VALUE", "VALUE is PHPSESSID"){|v| option[:session] = v}
+    opt.on("-i", "--id=VALUE", "Target User ID"){|v| option[:id] = v}
+    opt.parse!(ARGV)
+end
+
+if option[:id] == nil then
+    puts "target user id?"
+    user_id = STDIN.gets.chomp
+else
+    user_id = option[:id]
+end
+if option[:session] == nil then
+    puts "your PHPSESSID?"
+    phpsessid = STDIN.gets.chomp
+else
+    phpsessid = option[:session]
+end
+cookies={
+  #'personalization_id' => '"v1_hKYXzwe8wclGl/P4VPRwPw=="',
+  'PHPSESSID' => phpsessid
+}
+
+fi = FanboxItems.new(user_id,cookies)
+fi.get_all
